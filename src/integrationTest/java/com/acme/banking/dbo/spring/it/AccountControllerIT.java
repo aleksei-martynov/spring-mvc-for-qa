34203264b504854a3aa5dad6c671a40b8d4469bd
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-05-30 14:52:47 +0300

    Add interaction-based testing.

--- src/integrationTest/java/com/acme/banking/dbo/spring/it/AccountControllerIT.java
+++ src/integrationTest/java/com/acme/banking/dbo/spring/it/AccountControllerIT.java
@@ -10,14 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
 
 import static java.util.Arrays.asList;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -52,5 +53,13 @@ public class AccountControllerIT {
                 .andExpect(jsonPath("$.length()").value("1"))
                     .andExpect(jsonPath("$[0].amount").value("100.0"))
                     .andExpect(jsonPath("$[0].email").value("sa@sa.sa"));
+    }
+
+    @Test
+    public void shouldDeleteInRepoWhenDelete() throws Exception {
+        mockMvc.perform(delete("/api/accounts/1")
+                        .header("X-API-VERSION", "1"));
+
+        verify(accounts, times(1)).deleteById(anyLong());
     }
 }
